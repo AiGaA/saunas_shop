686ad61d9d8fafe972d8d0abd4da3edd67f81679
Aiga <aiga.andrejeva@gmail.com>
2024-01-25 21:29:39 +0000

    Added new fixtures and made database migration

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, StoveFeature, WindowFeature
 
 # Register your models here.
 
@@ -7,7 +7,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
         'name',
-        'category',
+        'stove_feature',
+        'window_feature',
         'price',
         'description',
     )
@@ -19,6 +20,22 @@ class CategoryAdmin(admin.ModelAdmin):
         'stove_type',
         'name',
     )
+
+class StoveFeatureAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'stove_type',
+        'price',
+    )
+
+class WindowFeatureAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'window_type',
+        'price',
+    )
     
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(StoveFeature)
+admin.site.register(WindowFeature)
--- products/models.py
+++ products/models.py
@@ -16,8 +16,33 @@ class Category(models.Model):
         return self.stove_type
 
 
+class StoveFeature(models.Model):
+    name = models.CharField(max_length=254)
+    stove_type = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    def get_stove_type(self):
+        return self.stove_type
+
+
+class WindowFeature(models.Model):
+    name = models.CharField(max_length=254)
+    window_type = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    def get_window_type(self):
+        return self.window_type
+
+
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    stove_feature = models.ForeignKey('StoveFeature', null=True, blank=True, default=None, on_delete=models.SET_NULL)
+    window_feature = models.ForeignKey('WindowFeature', null=True, blank=True, default=None, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
@@ -27,3 +52,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    @property
+    def total_price(self):
+        return sum(self.price * self.stove_feature.price * self.window_feature.price)
