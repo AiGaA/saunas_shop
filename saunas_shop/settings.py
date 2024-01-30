9d177d3e2e6967613fe62764854af43070718881
Aiga <aiga.andrejeva@gmail.com>
2024-01-30 02:33:25 +0000

    Fixed Database url issue

--- saunas_shop/settings.py
+++ saunas_shop/settings.py
@@ -131,7 +131,7 @@ WSGI_APPLICATION = 'saunas_shop.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse('DATABASE_URL')
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
