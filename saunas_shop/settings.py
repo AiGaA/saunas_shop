238a272ce1c6c51fb4c6168bb2b73b9250d374d4
Aiga <aiga.andrejeva@gmail.com>
2024-01-30 00:25:37 +0000

    installed whitenoise

--- saunas_shop/settings.py
+++ saunas_shop/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'saunas_shop.urls'
