0a0e5dac862595435b80a0a390c7b6c1d5969acb
Aiga <aiga.andrejeva@gmail.com>
2024-01-30 00:43:52 +0000

    Add whitenoise to the project

--- saunas_shop/wsgi.py
+++ saunas_shop/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saunas_shop.settings')
 
