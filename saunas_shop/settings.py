9a7c6b2a8d08c3a68d86ab2d16a09a57d53961a3
Aiga <aiga.andrejeva@gmail.com>
2024-01-30 00:09:09 +0000

    Fix allowed hosts to the app

--- saunas_shop/settings.py
+++ saunas_shop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-aigaa-saunasshop-q1kkehc03g6.ws-eu107.gitpod.io', 'https://saunas-shop-22fabe13bf89.herokuapp.com/']
+ALLOWED_HOSTS = ['saunas-shop-22fabe13bf89.herokuapp.com', '8000-aigaa-saunasshop-q1kkehc03g6.ws-eu107.gitpod.io', 'localhost']
 
 CSRF_TRUSTED_ORIGINS= ['https://8000-aigaa-saunasshop-q1kkehc03g6.ws-eu107.gitpod.io']
 
