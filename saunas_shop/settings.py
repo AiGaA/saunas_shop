9a84b415a134fc9f0fbb3a712e6c99ac137a8198
Aiga <aiga.andrejeva@gmail.com>
2024-03-31 01:20:54 +0000

    Debug turned off

--- saunas_shop/settings.py
+++ saunas_shop/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['saunas-shop-22fabe13bf89.herokuapp.com', '8000-aigaa-saunasshop-q1kkehc03g6.ws-eu107.gitpod.io', '8000-aigaa-saunasshop-4fkabhyzkw6.ws-eu108.gitpod.io', '8000-aigaa-saunasshop-4fkabhyzkw6.ws-eu110.gitpod.io', '8000-aigaa-saunasshop-ahaqq4wfuic.ws-eu110.gitpod.io', 'localhost']
 
