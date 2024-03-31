0d4efb5c5927db32b7aa3f356954a29f3d83808f
Aiga <aiga.andrejeva@gmail.com>
2024-03-31 02:30:25 +0000

    Implement AWS

--- saunas_shop/settings.py
+++ saunas_shop/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['saunas-shop-22fabe13bf89.herokuapp.com', '8000-aigaa-saunasshop-q1kkehc03g6.ws-eu107.gitpod.io', '8000-aigaa-saunasshop-4fkabhyzkw6.ws-eu108.gitpod.io', '8000-aigaa-saunasshop-4fkabhyzkw6.ws-eu110.gitpod.io', '8000-aigaa-saunasshop-ahaqq4wfuic.ws-eu110.gitpod.io', 'localhost']
 
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
         
     'crispy_forms',
+    'storages',
 
 ]
 
@@ -184,6 +185,26 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'saunas-shop'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
